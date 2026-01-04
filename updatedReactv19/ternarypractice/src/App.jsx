53674a53ee4cf8456178fee870cc85c760daba10
uzma-khan98 <uzma.uk.1998@gmail.com>
2026-01-04 08:11:03 +0500

    Removed default styling

--- updatedReactv19/ternarypractice/src/App.jsx
+++ updatedReactv19/ternarypractice/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ArrayState from "./components/ArrayState";
+import TogglingState from "./components/TogglingState";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  /**
+   * Challenge: Replace the if/else below with a ternary
+   * to determine the text that should display on the page
+   */
+  // const isGoingOut = false;
+
+  // let answer = isGoingOut === true ? "Yes" : "No" // 👈 Use ternary here
+
+  // Remove the code below 👇 once your ternary is done
+  // if (isGoingOut === true) {
+  //   answer = "Yes"
+  // } else {
+  //   answer = "No"
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      
+      {/* <h1 className="title">Do I feel like going out tonight?</h1>
+     
+      <button className="value">{isGoingOut ? "Yes" : "No"}</button> */}
+
+      {/* <TogglingState /> */}
+      <ArrayState/>
+    </main>
   )
 }
-
-export default App
